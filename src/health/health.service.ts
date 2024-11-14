b0d49c3e476d8e82a76983345500804a92d525cb
shashwotbhattarai <shashwot.tech@gmail.com>
2024-11-14 18:59:34 +0545

    testing sonar in ci pipeline

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -4,6 +4,8 @@ import { HealthResponseDTO } from './dto/healthResponse.dto';
 @Injectable()
 export class HealthService {
 	checkHealth(): HealthResponseDTO {
+		const password= 'test-5';
+		Logger.log('password', password);
 		Logger.log('Health check requested: Nest Setup is alive test-5');
 		return {
 			message: 'Health check successful',
