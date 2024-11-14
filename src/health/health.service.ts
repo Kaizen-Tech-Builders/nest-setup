ae5af9f5b199bb54bd9d1a57b3a6e1d27f9f2894
shashwotbhattarai <shashwot.tech@gmail.com>
2024-11-14 19:10:06 +0545

    ok

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -4,8 +4,6 @@ import { HealthResponseDTO } from './dto/healthResponse.dto';
 @Injectable()
 export class HealthService {
 	checkHealth(): HealthResponseDTO {
-		const password = 'test-5';
-		Logger.log('password', password);
 		Logger.log('Health check requested: Nest Setup is alive test-5');
 		return {
 			message: 'Health check successful',
