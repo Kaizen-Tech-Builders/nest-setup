7b19ca50adbcefe70eba28e4f47fed818664e378
shashwotbhattarai <shashwot.tech@gmail.com>
2024-12-14 17:40:42 +0545

    test ci lint

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HealthResponseDTO } from './dto/healthResponse.dto';
-import { HealthCheckResponse } from '@nestjs/terminus';
 
 @Injectable()
 export class HealthService {
 	checkHealth(): HealthResponseDTO {
 		Logger.log('Health check requested: Nest Setup is alive test-1');
+		console.log('Health check requested: Nest Setup is alive test-2');
 		return {
 			message: 'Health check successful',
 			time: new Date().toISOString(),
