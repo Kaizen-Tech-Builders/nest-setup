28ad01da04e7b2d88ebbe443ad2eaa2b98de2127
shashwotbhattarai <shashwot.tech@gmail.com>
2024-12-14 17:53:25 +0545

    everything fine

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -4,9 +4,7 @@ import { HealthResponseDTO } from './dto/healthResponse.dto';
 @Injectable()
 export class HealthService {
 	checkHealth(): HealthResponseDTO {
-		Logger.log('Health check requested: Nest Setup is alive test-1');
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const password="uhf6663khjd77";
+		Logger.log('Health check requested: Nest Setup is alive');
 		return {
 			message: 'Health check successful',
 			time: new Date().toISOString(),
